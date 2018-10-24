153eb8924d7bcd54ba84094a0ee2a5c43ed4baef
Patrick Marshall <patrickmarshall@outlook.sg>
2018-10-24 12:12:31 +0700

    Initial Commit

---
+++ ComponentBased/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComponentBased
+//
+//  Created by Patrick Marshall on 24/10/18.
+//  Copyright © 2018 Patrick Marshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
